04c6138a8ceedf9c02b4a59b57cf5fba400491ff
xaq <20221819402019@school.edu.cn>
2026-02-10 15:37:47 +0800

    tab导航栏设计

--- lib/pages/Main/index.dart
+++ lib/pages/Main/index.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:hm_shop/pages/Cart/index.dart';
+import 'package:hm_shop/pages/Category/index.dart';
+import 'package:hm_shop/pages/Home/index.dart';
+import 'package:hm_shop/pages/Mine/index.dart';
 
 class MainPage extends StatefulWidget {
   MainPage({Key? key}) : super(key: key);
@@ -6,16 +10,57 @@ class MainPage extends StatefulWidget {
   @override
   _MainPageState createState() => _MainPageState();
 }
-
+int _currentIndex=0;
 class _MainPageState extends State<MainPage> {
+  final List<Map<String, String>> _tabList=[{
+    "icon":"lib/assets/mainIcon.png",
+    "activeIcon":"lib/assets/activeIcon.png",
+    "text":"首页"},
+    {
+    "icon":"lib/assets/mainIcon.png",
+    "activeIcon":"lib/assets/activeIcon.png",
+    "text":"分类"},
+    {
+    "icon":"lib/assets/mainIcon.png",
+    "activeIcon":"lib/assets/activeIcon.png",
+    "text":"购物车"},
+    {
+    "icon":"lib/assets/mainIcon.png",
+    "activeIcon":"lib/assets/activeIcon.png",
+    "text":"我的"},
+    ];
+List<BottomNavigationBarItem> _getTabBarWidget(){
+  return List.generate(_tabList.length, (index){
+    return BottomNavigationBarItem(
+      icon:Image.asset(_tabList[index]["icon"]!, width:30, height:30),
+      activeIcon:Image.asset(_tabList[index]["activeIcon"]!, width:30, height:30),
+      label:_tabList[index]["text"]!,
+    );
+  });
+}
+List<Widget> _getViewWidget(){
+  return [HomeView(),CategoryView(),CartView(),MineView()];
+}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title:Text('主页面'),
+      body:SafeArea(
+        child: IndexedStack(
+          index: _currentIndex,
+          children: _getViewWidget(),
+        )
       ),
-      body:Center(
-        child: Text('主页面'),
+      bottomNavigationBar: BottomNavigationBar(
+        showUnselectedLabels: true,
+        unselectedItemColor: Colors.grey,
+        selectedItemColor: Colors.black,
+        onTap:(int index){
+          setState(() {
+            _currentIndex=index;
+          });
+        },
+        currentIndex:_currentIndex,
+        items: _getTabBarWidget(),
       ),
     );
   }
